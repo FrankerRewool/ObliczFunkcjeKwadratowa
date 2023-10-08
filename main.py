b30dbc6be4e620a14b1a7684797160e33ce8b29d
FrankerRewool <113792398+FrankerRewool@users.noreply.github.com>
2023-10-09 00:16:49 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+print("wprowadź funkcje kwadratową")
+a = int(input("wprowadż a")) # a
+b = int(input("wprowadż b")) # b
+c = int(input("wprowadż c")) # c
+d = (b ** 2)-4*a*c # wylicza delte
+print("Δ =", d) # wyświetla delte
+pd = print("√Δ =", d**(1/2)) # funkcja wykonuje obliczanie pierwiastka z delty
+x1 = (-b + (d ** 0.5)) / (2 * a) # x1
+x2 = (-b - (d ** 0.5)) / (2 * a) # x2
+print(x1, x2)
+print("wzory viete'a")
+
